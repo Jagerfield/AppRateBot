eb9ec317a410e8140245e820d4657cb989d03082
TrevorJagerfield[user] <jagerfield@gmail.com>
2017-07-16 18:30:32 +0200

    Commit

--- appratebotlib/src/main/java/ratemyapp/jagerfield/appratebotlib/builders/UsageMonitor.java
+++ appratebotlib/src/main/java/ratemyapp/jagerfield/appratebotlib/builders/UsageMonitor.java
@@ -6,6 +6,14 @@ import java.util.TimeZone;
 import ratemyapp.jagerfield.appratebotlib.Utils.CLib;
 import ratemyapp.jagerfield.appratebotlib.Utils.PreferenceUtil;
 
+/**
+ * Two main functions
+ * 1- checkPreferencesLastUsageDate(context): Check that there is a valid lastUsageDate stored in the Preferences,
+ *    if missing or first time running then reset usage count and last usage values.
+ *
+ * 2- updateUsageInformation(): Compare the current date to the saved last usage date. If the difference is a day or more,
+ *    then update usage count and last usage date values in Preferences.
+ */
 public class UsageMonitor
 {
     private Context context;
@@ -24,31 +32,9 @@ public class UsageMonitor
     public void updateUsageInformation()
     {
         Calendar cal = getCurrentCal();
-        int yearCal = cal.get(Calendar.YEAR);
-        int monthCal = cal.get(Calendar.MONTH);
-
-        int weekCal = cal.get(Calendar.WEEK_OF_YEAR);
-        int dayOfMonthCal = cal.get(Calendar.DAY_OF_MONTH);
-        int dayOfWeekCal = cal.get(Calendar.DAY_OF_WEEK);
-
-        int hoursCal = cal.get(Calendar.HOUR_OF_DAY);
-        int minutesCal = cal.get(Calendar.MINUTE);
-        int secondsCal = cal.get(Calendar.SECOND);
-        int milliSecCal = cal.get(Calendar.MILLISECOND);
-
-
         Calendar lastUsageCal = getLastUsageCal(context);
-        int yearLastUsageCal = lastUsageCal.get(Calendar.YEAR);
-        int monthLastUsageCal = lastUsageCal.get(Calendar.MONTH);
 
-        int weekLastUsageCal = lastUsageCal.get(Calendar.WEEK_OF_YEAR);
-        int dayOfMonthLastUsageCal = lastUsageCal.get(Calendar.DAY_OF_MONTH);
-        int dayOfWeekLastUsageCal = lastUsageCal.get(Calendar.DAY_OF_WEEK);
-
-        int hoursLastUsageCal = lastUsageCal.get(Calendar.HOUR_OF_DAY);
-        int minutesLastUsageCal = lastUsageCal.get(Calendar.MINUTE);
-        int secondsLastUsageCal = lastUsageCal.get(Calendar.SECOND);
-        int milliSecLastUsageCal = lastUsageCal.get(Calendar.MILLISECOND);
+        showCalendarsValues(cal, lastUsageCal);
 
         int i = cal.compareTo(lastUsageCal);
 
@@ -123,6 +109,33 @@ public class UsageMonitor
     {
         long lastSavedDate = PreferenceUtil.getLong(context, CLib.IKEYS.KEY_LAST_USAGE_DATE, 0l);
         if (lastSavedDate==0) { resetUsageAndDate(context); }
+    }
+
+    private void showCalendarsValues(Calendar cal, Calendar lastUsageCal) {
+        int yearCal = cal.get(Calendar.YEAR);
+        int monthCal = cal.get(Calendar.MONTH);
+
+        int weekCal = cal.get(Calendar.WEEK_OF_YEAR);
+        int dayOfMonthCal = cal.get(Calendar.DAY_OF_MONTH);
+        int dayOfWeekCal = cal.get(Calendar.DAY_OF_WEEK);
+
+        int hoursCal = cal.get(Calendar.HOUR_OF_DAY);
+        int minutesCal = cal.get(Calendar.MINUTE);
+        int secondsCal = cal.get(Calendar.SECOND);
+        int milliSecCal = cal.get(Calendar.MILLISECOND);
+
+
+        int yearLastUsageCal = lastUsageCal.get(Calendar.YEAR);
+        int monthLastUsageCal = lastUsageCal.get(Calendar.MONTH);
+
+        int weekLastUsageCal = lastUsageCal.get(Calendar.WEEK_OF_YEAR);
+        int dayOfMonthLastUsageCal = lastUsageCal.get(Calendar.DAY_OF_MONTH);
+        int dayOfWeekLastUsageCal = lastUsageCal.get(Calendar.DAY_OF_WEEK);
+
+        int hoursLastUsageCal = lastUsageCal.get(Calendar.HOUR_OF_DAY);
+        int minutesLastUsageCal = lastUsageCal.get(Calendar.MINUTE);
+        int secondsLastUsageCal = lastUsageCal.get(Calendar.SECOND);
+        int milliSecLastUsageCal = lastUsageCal.get(Calendar.MILLISECOND);
     }
 }
 
